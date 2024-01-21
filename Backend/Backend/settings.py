1ffc153c89f87775671238e602b70f741d5c5293
MeryHarikaG <gaddam.mery.harika01@gmail.com>
2024-01-21 23:33:18 +0530

    Changed Staticfiles path

--- Backend/Backend/settings.py
+++ Backend/Backend/settings.py
@@ -134,8 +134,11 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+# STATICFILES_DIRS = [
+#     os.path.join(BASE_DIR, '../Frontend/build/static'),
+# ]
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, '../Frontend/build/static'),
+    BASE_DIR / 'Frontend/build/static',
 ]
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
